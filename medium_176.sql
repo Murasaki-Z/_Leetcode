a91620e524ba2206e08a34b43051a889fbc7bd07
thekillingjake <anand.abhishek961@gmail.com>
2023-06-01 23:10:14 +1000

    edit

--- medium_176.sql
+++ medium_176.sql
@@ -3,30 +3,15 @@
 
 
 
-
-
-#Redshift compatible
- select SecondHighestSalary
- from (
- select RANK() OVER (ORDER BY salary DESC) as rnk, 
-  case when rnk = 2 then salary else null end as SecondHighestSalary
- from Employee
- )subq
- where SecondHighestSalary is not null
-
-select case when rnk = 2 then salary else null end as SecondHighestSalary
-from (
-select *, RANK() OVER (ORDER BY salary DESC) as rnk
-from Employee)subq
-order by SecondHighestSalary desc
-limit 1
-
-
-WITH ranked_employees AS (
-  SELECT *, RANK() OVER (ORDER BY salary DESC) AS rnk
-  FROM Employee
+WITH ranked_salaries AS (
+  SELECT salary, RANK() OVER (ORDER BY salary DESC) AS rnk
+  FROM (
+    SELECT DISTINCT salary FROM Employee
+  ) AS inner_table
 )
 SELECT CASE WHEN rnk = 2 THEN salary ELSE NULL END AS SecondHighestSalary
-FROM ranked_employees
+FROM ranked_salaries
 ORDER BY SecondHighestSalary DESC
-LIMIT 1;
+LIMIT 1; #Replace limit and distinct with rn if performance is not a conncern
+
+#simpler solution -- check if salary < max(salary) limit 1 after order

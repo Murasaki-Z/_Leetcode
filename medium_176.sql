727e1f6a827270d42e49c9db339df9f034864890
thekillingjake <anand.abhishek961@gmail.com>
2023-06-01 22:52:04 +1000

    edit

--- medium_176.sql
+++ medium_176.sql
@@ -6,13 +6,13 @@
 
 
 #Redshift compatible
-# select SecondHighestSalary
-# from (
-# select RANK() OVER (ORDER BY salary DESC) as rnk, 
-# # case when rnk = 2 then salary else null end as SecondHighestSalary
-# from Employee
-# )subq
-# where SecondHighestSalary is not null
+ select SecondHighestSalary
+ from (
+ select RANK() OVER (ORDER BY salary DESC) as rnk, 
+  case when rnk = 2 then salary else null end as SecondHighestSalary
+ from Employee
+ )subq
+ where SecondHighestSalary is not null
 
 select case when rnk = 2 then salary else null end as SecondHighestSalary
 from (
@@ -20,3 +20,13 @@ select *, RANK() OVER (ORDER BY salary DESC) as rnk
 from Employee)subq
 order by SecondHighestSalary desc
 limit 1
+
+
+WITH ranked_employees AS (
+  SELECT *, RANK() OVER (ORDER BY salary DESC) AS rnk
+  FROM Employee
+)
+SELECT CASE WHEN rnk = 2 THEN salary ELSE NULL END AS SecondHighestSalary
+FROM ranked_employees
+ORDER BY SecondHighestSalary DESC
+LIMIT 1;
